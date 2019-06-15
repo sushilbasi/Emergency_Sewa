4d3344f313dfa02c02c35b3507d7483e90ed01cb
Sushil Basi <basisushil@gmail.com>
2019-06-15 15:46:36 +0545

    updated

--- app/src/main/java/com/college/emergencysewa/activity_map.java
+++ app/src/main/java/com/college/emergencysewa/activity_map.java
@@ -1,17 +1,26 @@
 package com.college.emergencysewa;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,6 +53,12 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
 
+import static android.support.constraint.Constraints.TAG;
+
+import android.location.LocationManager;
+import android.location.Location;
+
+import com.google.android.gms.location.LocationServices;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,8 +71,6 @@ public class activity_map extends Fragment {
     private ScaleBarOverlay mScaleBarOverlay;
     private Road road;
     GeoPoint startPoint;
-
-
 
     public activity_map() {
         // Required empty public constructor
@@ -91,18 +104,15 @@ public class activity_map extends Fragment {
 //        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
 
 
-
-
         //We can move the map on default view point. For this, we need access to map controller
 
-        double latitude = 27.693123;
-        double longitude =  85.321023;
+//        double latitude = 27.693123;
+//        double longitude = 85.321023;
         IMapController mapController = map.getController();
         mapController.setZoom(18);
 
-        startPoint = new GeoPoint(latitude,longitude);
+        startPoint = new GeoPoint(27.683126, 85.348968);
         mapController.setCenter(startPoint);
-
 
 
         //show Compass
@@ -116,35 +126,55 @@ public class activity_map extends Fragment {
         myLocation.setLocationUpdateMinDistance(5);
         myLocation.addLocationSource(LocationManager.GPS_PROVIDER);
         myLocation.addLocationSource(LocationManager.NETWORK_PROVIDER);
+
         mLocationOverlay = new MyLocationNewOverlay(myLocation, map);
+        Log.d(TAG, "THIS IS MAP LOCTATION");
+        System.out.println(mLocationOverlay);
         mLocationOverlay.enableMyLocation();
         mLocationOverlay.enableFollowLocation();
         mLocationOverlay.isFollowLocationEnabled();
         mLocationOverlay.isMyLocationEnabled();
         mLocationOverlay.setDrawAccuracyEnabled(true);
+
         map.getOverlays().add(this.mLocationOverlay);
         map.setClickable(true);
         map.setFocusable(true);
         map.setFocusableInTouchMode(true);
         mapController.setCenter(mLocationOverlay.getMyLocation());
         mapController.animateTo(mLocationOverlay.getMyLocation());
+        System.out.println(mLocationOverlay.getMyLocation());
+
+        LocationManager locationManager = (LocationManager)
+                getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
 
 
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        /////
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager
+                .getBestProvider(criteria, false));
+
+        double lati = location.getLatitude();
+        double longi = location.getLongitude();
+
+        System.out.println(lati + ","+longi);
+
+
         addMarker2(ctx,map,startPoint);
-
-
+        //Display Route between two points
         RoadManager roadManager = new OSRMRoadManager(this.getContext());
         ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
         waypoints.add(startPoint);
-        GeoPoint endPoint = new GeoPoint(27.671533, 85.354679);
+        GeoPoint endPoint = new GeoPoint(lati, longi);
         waypoints.add(endPoint);
-        addMarker(ctx,map,endPoint);
+        final double xMeters = ((GeoPoint) startPoint).distanceToAsDouble(endPoint);
+        double distance = startPoint.distanceToAsDouble(endPoint);
+        System.out.println(distance);
         Road road = null;
         try {
             road = roadManager.getRoad(waypoints);
-            Toast.makeText(getActivity(), "HELP IS HERE" + myLocation.getLastKnownLocation(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "You are here", Toast.LENGTH_SHORT).show();
         }catch (Exception e )
         {
             e.printStackTrace();
@@ -152,7 +182,17 @@ public class activity_map extends Fragment {
         if(road.mStatus != Road.STATUS_OK) {
             Toast.makeText(getActivity(), "Error when loading the road - status="+road.mStatus, Toast.LENGTH_SHORT).show();
         }
+        double d = road.mDuration; // get duration
+        double x = road.mLength; // get length
+        String st_timeLeft = String.format("%s min", String.valueOf(myRound(d / 60,3)));//format string
+        String st_dist = String.valueOf(myRound(x * 1000,2)) + " m";
+        System.out.println(st_timeLeft +","+st_dist);
+//        timeLeft.setText(st_timeLeft);
+//        distGoal.setText(st_dist);
         Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+        roadOverlay.setColor(getContext().getResources().getColor(R.color.colorAccent));
+        roadOverlay.setWidth(16);
+        roadOverlay.setGeodesic(false);
         map.getOverlays().add(roadOverlay);
         map.invalidate();
 
@@ -160,7 +200,14 @@ public class activity_map extends Fragment {
 
 //        addMarker(ctx,map,startPoint);
 
+
+
+
         return view;
+    }
+
+    static double myRound(double wert, int stellen) {
+        return  Math.round(wert * Math.pow(10, stellen)) / Math.pow(10, stellen);
     }
 
 
